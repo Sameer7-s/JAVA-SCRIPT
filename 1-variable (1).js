eb5c536909dc3f77aed092d168aedf64a7941151
Sameer <sameergoutam794@gmail.com>
2026-04-22 18:07:49 +0530

    variables in javascripts

---
+++ 1-variable (1).js
@@ -0,0 +1,39 @@
+/*===========Today's learnings===============
+
+console.log => to print output
+
+there are 3 types of variable:-
+1. let     => new variable, can be reassigned
+2. const   => const variable, can't be reassigned
+3. var     => old variable, don't use because of limitations
+
+note: trying to change const will give #error
+
+let vs const :- in let we can assign new value but in const we can't
+analogy: let is like balance and const is like bank account number
+
+let vs var: let is new variable and var is old variable, we don't use var because of limitations
+
+*/
+
+console.log ("Hello Rover")
+
+let bus = 12
+
+console.log (bus)
+
+bus = 14
+
+console.log(bus)
+
+let beta = "betabu"
+
+console.log (beta)
+
+const blu = 20
+
+console.log (blu)
+
+// blu = 10
+
+// console.log (blu)   // this is error, can't change const value but var value can be changed
